7df2e8ace0627631dcff5c58f06386a735f29b80
Josh Levy <joshaaronlevy@gmail.com>
2021-04-06 01:58:55 -0600

    "Working on photo upload"

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -6,49 +6,38 @@ import {
   Capacitor,
   FilesystemDirectory,
   CameraPhoto,
-  CameraSource
+  CameraSource,
 } from '@capacitor/core';
 import { Platform } from '@ionic/angular';
 import { v4 as uuid } from 'uuid';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
-
 const { Camera, Filesystem, Storage } = Plugins;
-
 const photoUrl = environment.photoUrl;
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
   photo: any;
   photos: any = [];
   uniqueId: string;
-  // tslint:disable-next-line: no-inferrable-types
   PHOTO_STORAGE: string = 'photos';
   platform: Platform;
-  uploadParams: { filepath: string; webviewPath: string; };
-
-  constructor(
-    platform: Platform,
-    private http: HttpClient
-  ) {
+  uploadParams: { filepath: string; webviewPath: string };
+  constructor(platform: Platform, private http: HttpClient) {
     this.platform = platform;
   }
-
   create(params: any) {
-    return this.http.post(photoUrl, params).subscribe(response => {
+    return this.http.post(photoUrl, params).subscribe((response) => {
       console.log(response);
       return response;
     });
   }
-
   requestAccess() {
     Camera.requestPermissions().then((response) => {
       return response;
     });
   }
-
   async addNewToGallery(id) {
     this.uniqueId = id;
     const capturedPhoto = await Camera.getPhoto({
@@ -57,75 +46,34 @@ export class PhotoService {
       width: 375,
       height: 360,
       quality: 100,
-      saveToGallery: true
+      saveToGallery: true,
     });
     console.log(capturedPhoto);
     const savedImageFile = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
     console.log(savedImageFile);
-
     // this.photos.unshift({
     //   filepath: '',
     //   webviewPath: capturedPhoto.webPath
     // });
     // console.log(this.photos);
-
     // Storage.set({
     //   key: this.PHOTO_STORAGE,
     //   value: JSON.stringify(this.photos)
     // });
   }
-
-  async savePicture(cameraPhoto: CameraPhoto) {
-    console.log(cameraPhoto);
-    const fileName = `Tag-${this.uniqueId}-1.jpg`;
-    const base64Data = await this.readAsBase64(cameraPhoto, fileName);
-    const savedFile = await Filesystem.writeFile({
-      path: fileName,
-      data: base64Data,
-      directory: FilesystemDirectory.Data
-    });
-    console.log(savedFile);
-    if (this.platform.is('hybrid')) {
-      return {
-        filepath: savedFile.uri,
-        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-      };
-    } else {
-      this.uploadParams = {
-        filepath: fileName,
-        // webviewPath: cameraPhoto.webPath,
-        webviewPath: fileName
-      };
-      this.photo = cameraPhoto;
-      console.log(this.photo);
-      console.log(this.uploadParams);
-      return this.sendPostRequest();
-    }
-  }
-
   async readAsBase64(cameraPhoto: CameraPhoto, fileName) {
     if (this.platform.is('hybrid')) {
       const file = await Filesystem.readFile({
-        path: cameraPhoto.path
+        path: cameraPhoto.path,
       });
       return file.data;
     } else {
       const response = await fetch(fileName);
       const blob = await response.blob();
-      return await this.convertBlobToBase64(blob) as string;
+      return (await this.convertBlobToBase64(blob)) as string;
     }
   }
-
-  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-    reader.readAsDataURL(blob);
-  })
-
   async loadSaved() {
     const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
     this.photos = JSON.parse(photoList.value) || [];
@@ -133,28 +81,50 @@ export class PhotoService {
       for (const photo of this.photos) {
         const readFile = await Filesystem.readFile({
           path: photo.filepath,
-          directory: FilesystemDirectory.Data
+          directory: FilesystemDirectory.Data,
         });
         photo.webviewPath = `data:image/jpg;base64,${readFile.data}`;
       }
     }
   }
-
-  sendPostRequest() {
-    const fullFileName = `Tag-${this.uniqueId}-1.jpg`;
-    const url = 'https://photos.homecards.com/admin/uploads/app/';
-    const headers = new HttpHeaders()
-      .set('x-token', this.uniqueId);
-    const body = {
-      fileName: fullFileName,
-      mimeType: 'multipart/form-data'
-    };
-    return this.http
-      .post(url, { file: body }, { headers })
-      .subscribe(res => {
-        console.log(res);
-      }, err => {
-        console.log(err);
-      });
+  async savePicture(cameraPhoto: CameraPhoto) {
+    const file = this.dataUriToFile(cameraPhoto.dataUrl, `Tag-${this.uniqueId}-1.jpg`)
+    return await this.sendPostRequest(file);
   }
+  sendPostRequest(file: File) {
+    const url = 'https://photos.homecards.com/admin/uploads/app/';
+    const headers = new HttpHeaders({
+      'x-token': this.uniqueId,
+    });
+    const payload = new FormData();
+    payload.append('file', file);
+    return this.http.post(url, payload, { headers }).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  dataUriToFile(dataUri: string, filename: string) {
+    const [mimeInfo, base64] = dataUri.split(',');
+    const mime = mimeInfo.match(/:(.*?);/)[1];
+    const bstr = atob(base64);
+    let n = bstr.length;
+    const bits = new Uint8Array(n);
+    while (n--) {
+      bits[n] = bstr.charCodeAt(n);
+    }
+    return new File([bits], filename, { type: mime });
+  }
+  convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    })
 }
