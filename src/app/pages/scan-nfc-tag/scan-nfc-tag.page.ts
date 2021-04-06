330974609d42461c5554c69a5d62f62f62ae6c28
Josh Levy <joshaaronlevy@gmail.com>
2021-04-06 14:06:42 -0600

    "Working on tag scanning"

--- src/app/pages/scan-nfc-tag/scan-nfc-tag.page.ts
+++ src/app/pages/scan-nfc-tag/scan-nfc-tag.page.ts
@@ -12,6 +12,7 @@ import { NFC, Ndef } from '@ionic-native/nfc/ngx';
   ]
 })
 export class ScanNFCTagPage implements OnInit {
+  scanning: boolean;
   readerMode$: any;
   previousRoute: string;
   theme: string;
@@ -23,6 +24,7 @@ export class ScanNFCTagPage implements OnInit {
   ) { }
 
   async ngOnInit() {
+    this.scanning = true;
     if (localStorage.getItem('theme') === 'light') {
       this.theme = 'light';
     } else {
@@ -44,6 +46,45 @@ export class ScanNFCTagPage implements OnInit {
     // On iOS, a NFC reader session takes control from your app while scanning tags then returns a tag
     try {
       const tag = await this.nfc.scanNdef();
+      console.log(JSON.stringify(tag));
+    } catch (err) {
+      console.log('Error reading tag', err);
+    }
+  }
+
+  async startScan() {
+    this.scanning = true;
+    const flags = this.nfc.FLAG_READER_NFC_A || this.nfc.FLAG_READER_NFC_V;
+    // tslint:disable-next-line: deprecation
+    // this.readerMode$ = this.nfc.readerMode(flags).subscribe(tag => {
+    //   console.log(JSON.stringify(tag));
+    // });
+    this.readerMode$ = this.nfc.readerMode(flags).subscribe(
+      tag => console.log(JSON.stringify(tag)),
+      err => console.log('Error reading tag', err)
+    );
+
+    // Read NFC Tag - iOS
+    // On iOS, a NFC reader session takes control from your app while scanning tags then returns a tag
+    try {
+      // this.nfc.addNdefListener(tag => {
+      //   console.log(tag);
+      // });
+      this.nfc.addNdefListener().subscribe(
+        tag => console.log(JSON.stringify(tag)),
+        err => console.log('Error reading tag', err)
+      );
+      const tag = await this.nfc.scanNdef();
+      console.log(JSON.stringify(tag));
+    } catch (err) {
+      console.log('Error reading tag', err);
+    }
+  }
+
+  async stopScan() {
+    try {
+      const tag = await this.nfc.cancelScan();
+      this.scanning = false;
       console.log(JSON.stringify(tag));
     } catch (err) {
       console.log('Error reading tag', err);
